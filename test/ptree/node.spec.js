284c51100e41dd482a1f5b2dfe011c8c95e4573e
Diego Borges <drborges.cic@gmail.com>
2017-12-14 10:26:58 -0200

    Wrap Node specs within a describe block for #get

--- test/ptree/node.spec.js
+++ test/ptree/node.spec.js
@@ -4,33 +4,35 @@ import { expect } from "chai"
 import Tree from "../../src/ptree"
 
 describe("Node", () => {
-  it("allows accessing methods only defined on the proxied target value", () => {
-    const tree = new Tree({
-      users: [
-        { name: "Diego" }
-      ]
+  describe("#get", () => {
+    it("allows accessing methods only defined on the proxied target value", () => {
+      const tree = new Tree({
+        users: [
+          { name: "Diego" }
+        ]
+      })
+
+      expect(tree.root.users.hasOwnProperty(0)).to.eq(true)
     })
 
-    expect(tree.root.users.hasOwnProperty(0)).to.eq(true)
-  })
+    it("does not evaluate 'falsy' props as 'undefined'", () => {
+      const tree = new Tree({
+        active: false,
+        emptyText: "",
+        zero: 0,
+      })
 
-  it("does not evaluate 'falsy' props as 'undefined'", () => {
-    const tree = new Tree({
-      active: false,
-      emptyText: "",
-      zero: 0,
+      expect(tree.root.active).to.eq(false)
+      expect(tree.root.emptyText).to.eq("")
+      expect(tree.root.zero).to.eq(0)
     })
 
-    expect(tree.root.active).to.eq(false)
-    expect(tree.root.emptyText).to.eq("")
-    expect(tree.root.zero).to.eq(0)
-  })
+    it("returns 'null' for null props in target", () => {
+      const tree = new Tree({
+        data: null,
+      })
 
-  it("returns 'null' for null props in target", () => {
-    const tree = new Tree({
-      data: null,
+      expect(tree.root.data).to.eq(null)
     })
-
-    expect(tree.root.data).to.eq(null)
   })
 })
