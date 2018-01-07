710fdb141cfd59fae9d3106967146f14ab0ce440
Diego Borges <drborges.cic@gmail.com>
2018-01-06 22:20:25 -0200

    Move path specs into its own module

--- test/path/cached.spec.js
+++ test/path/cached.spec.js
@@ -1,7 +1,7 @@
 import sinon from "sinon"
 import { expect } from "chai"
 
-import cached from "../src/path/cached"
+import cached from "../../src/path/cached"
 
 describe("@cached", () => {
 
--- test/path/path.spec.js
+++ test/path/path.spec.js
@@ -1,5 +1,5 @@
 import { expect } from "chai"
-import Path from "../src/path"
+import Path from "../../src/path"
 
 describe("Path", () => {
   describe("#toString", () => {
